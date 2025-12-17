435529c61923104fd49c64a8544feab666ce92a6
Hermela Mulugeta Jemema <heriri2525@gmail.com>
2025-12-17 07:00:24 +0300

    Add FizzBuzz implementation in fizzbuzz.dart

---
+++ week1_tasks/fizzbuzz.dart
@@ -0,0 +1,12 @@
+void main(){
+int num = 30;
+if(num % 3 ==0 &&num % 5 == 0){
+print("fizzbuzz");}
+else if (num % 3 == 0){
+print("fizz");}
+else if (num % 5 ==0){
+print("buzz");
+}
+else 
+print("$num is not divisible either 3 or 5");
+}
